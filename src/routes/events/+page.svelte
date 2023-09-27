271f59eba381673246a331c5da187e1d3e2597bf
bmehder <bmehder@gmail.com>
2023-09-27 14:03:17 -0400

    show all events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,8 +1,17 @@
 <script>
   export let data
 
-  const startDate = x =>
+  const getStartDate = x =>
     new Date(data.events[x]._bd_events_datetime).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+  
+    const getEndDate = x =>
+    new Date(data.events[x]._bd_events_datetime_end).toLocaleDateString('en-US', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
@@ -15,13 +24,15 @@
   const sortDescByDate = (a, b) =>
     new Date(b._bd_events_datetime) - new Date(a._bd_events_datetime)
 
-  const upcommingEvents = data.events
+  const upcommingEvents = [...data.events]
     .sort(sortDescByDate)
     .filter(x => new Date(x._bd_events_datetime_end) >= today)
-  
-  const pastEvents = data.events
+
+  const pastEvents = [...data.events]
     .sort(sortDescByDate)
     .filter(x => new Date(x._bd_events_datetime_end || x._bd_events_datetime) < today)
+
+  $: if (pastEvents.length > 6) upcommingEvents.length = 6
 </script>
 
 <svelte:head>
@@ -38,65 +49,75 @@
   <h2>Upcomming Events</h2>
 
   {#if upcommingEvents.length > 0}
-  <div class="auto-grid">
-    {#each upcommingEvents as event, idx}
-      <article class="flow">
-        {#if event._embedded?.['wp:featuredmedia']?.[0]}
-          <a href="/events/{event.slug}">
-            <img
-              class="square"
-              src={event._embedded?.['wp:featuredmedia']?.[0].source_url}
-              alt={event.title.rendered}
-            />
-          </a>
-        {/if}
-        <div class="flow">
-          <h2><a href="/events/{event.slug}">{@html event.title.rendered}</a></h2>
-          <p>
-            <a class="date" href="/events/{event.slug}">
-              <time>{startDate(idx)}</time><br />
+    <div class="auto-grid">
+      {#each upcommingEvents as event, idx}
+        <article class="flow">
+          {#if event._embedded?.['wp:featuredmedia']?.[0]}
+            <a href="/events/{event.slug}">
+              <img
+                class="square"
+                src={event._embedded?.['wp:featuredmedia']?.[0].source_url}
+                alt={event.title.rendered}
+              />
             </a>
-          </p>
-          <div>{@html event.excerpt.rendered}</div>
-        </div>
-        <a href="/events/{event.slug}" class="button">Read More</a>
-      </article>
-    {/each}
-  </div>
+          {/if}
+          <div class="flow">
+            <h2><a href="/events/{event.slug}">{@html event.title.rendered}</a></h2>
+            <p>
+              <a class="date" href="/events/{event.slug}">
+                <time>{getStartDate(idx)}</time><br />
+                {#if event._bd_events_datetime_end}
+                  <small>to</small><br />
+                  <time>{getEndDate(idx)}</time>
+                {/if}
+              </a>
+            </p>
+            <div>{@html event.excerpt.rendered}</div>
+          </div>
+          <a href="/events/{event.slug}" class="button">Read More</a>
+        </article>
+      {/each}
+    </div>
   {:else}
     <h3>No upcommimg events</h3>
   {/if}
 </section>
 
+<hr>
+
 <section class="flow">
   <h2>Past Events</h2>
 
   {#if pastEvents.length > 0}
-  <div class="auto-grid">
-    {#each pastEvents as event, idx}
-      <article class="flow">
-        {#if event._embedded?.['wp:featuredmedia']?.[0]}
-          <a href="/events/{event.slug}">
-            <img
-              class="square"
-              src={event._embedded?.['wp:featuredmedia']?.[0].source_url}
-              alt={event.title.rendered}
-            />
-          </a>
-        {/if}
-        <div class="flow">
-          <h2><a href="/events/{event.slug}">{@html event.title.rendered}</a></h2>
-          <p>
-            <a class="date" href="/events/{event.slug}">
-              <time>{startDate(idx)}</time><br />
+    <div class="auto-grid">
+      {#each pastEvents as event, idx}
+        <article class="flow">
+          {#if event._embedded?.['wp:featuredmedia']?.[0]}
+            <a href="/events/{event.slug}">
+              <img
+                class="square"
+                src={event._embedded?.['wp:featuredmedia']?.[0].source_url}
+                alt={event.title.rendered}
+              />
             </a>
-          </p>
-          <div>{@html event.excerpt.rendered}</div>
-        </div>
-        <a href="/events/{event.slug}" class="button">Read More</a>
-      </article>
-    {/each}
-  </div>
+          {/if}
+          <div class="flow">
+            <h2><a href="/events/{event.slug}">{@html event.title.rendered}</a></h2>
+            <p>
+              <a class="date" href="/events/{event.slug}">
+                <time>{getStartDate(idx)}</time><br />
+                {#if event._bd_events_datetime_end}
+                  <small>to</small><br />
+                  <time>{getEndDate(idx)}</time>
+                {/if}
+              </a>
+            </p>
+            <div>{@html event.excerpt.rendered}</div>
+          </div>
+          <a href="/events/{event.slug}" class="button">Read More</a>
+        </article>
+      {/each}
+    </div>
   {:else}
     <h3>No past events</h3>
   {/if}
