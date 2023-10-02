af0d2075256caacc22854897d0653c45d9bb86da
bmehder <bmehder@gmail.com>
2023-10-02 11:11:51 -0400

    frank curvin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 
   // export let data
 
-  const mobileThreshold = 960
+  const mobileThreshold = 1024
 </script>
 
 <svelte:head>
---
+++ src/routes/about-restore-first-health/frank-curvin-md/+page.svelte
@@ -0,0 +1,46 @@
+<section class="flow">
+  <h1 class="title">Frank Curvin, MD</h1>
+  <h2>Georgia</h2>
+
+  <div class="float-right">
+    <img
+      src="/georgia/frank-curvin-medical-director-restore-first-health.jpg"
+      alt="Frank Curvin, MD"
+    />
+  </div>
+  <p>
+    Dr. Curvin has over 20 years experience serving as a medical director and clinician
+    for post-acute care practices in Georgia. As Medical Director at Restore First Health,
+    Dr. Curvin manages all patient care for the practice, overseeing and collaborating in
+    the clinical protocols provided by the Nurse Practitioners. Before joining Restore
+    First Health in 2022, he was regional medical director for Institutional Special Needs
+    Program and Housecall & Telemedicine Practice for
+    <a href="https://heal.com/" target="_blank">Heal Doctors</a> in Atlanta, GA.
+  </p>
+  <p>
+    Dr. Curvin has a passion for patient care focused on medically proven therapies that
+    bring greater quality of life to his patients. With over two decades in family
+    medicine and managing complex care for patients, he continues to connect with his
+    patients and their families to understand barriers to their care, and present
+    collaborative solutions for improved health to all his patients.
+  </p>
+  <p>
+    He is a graduate of New Jersey Medical School at The University of Medicine and
+    Dentistry in Newark, NJ. Dr. Curvin completed his Family Medicine internship at Harbor
+    - UCLA Medical Center in Torrance, CA and residency training and fellowship at Emory
+    University - Crawford Long Medical Center in Atlanta, GA.
+  </p>
+</section>
+
+<style>
+  /* .float-right {
+    max-width: 300px;
+    margin-inline-start: var(--double-size);
+  } */
+
+  /* @media (min-width: 48em) {
+    .float-right img {
+      width: 300px;
+    }
+  } */
+</style>
