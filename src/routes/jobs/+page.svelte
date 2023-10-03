b0e6e6cad03ba7fd4825444fd907498e018677a5
bmehder <bmehder@gmail.com>
2023-10-03 15:26:25 -0400

    new jobs layout

--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -9,24 +9,105 @@
 <section class="flow">
   <h1 class="title">Job Opportunities</h1>
   <h2>New Jersey</h2>
-  <p>
-    <strong>Nurse Practitioner</strong><br />
-    Cranbury, NJ<br />
-    Salary: $140 - $150K - Full-time
-  </p>
-  <a
-    href="https://restorefirsthealthduluth.applytojob.com/apply/3yXDKoYkEi/Nurse-Practitioner"
-    class="button">Apply Now</a
-  >
+  <div class="auto-grid">
+    <div class="flow flex">
+      <p>
+        <strong>Nurse Practitioner</strong><br />
+        Cranbury, NJ<br />
+        Salary: $140k - $150K - Full-time
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/3yXDKoYkEi/Nurse-Practitioner"
+        target="_blank">Apply Now</a
+      >
+    </div>
+
+    <div class="flow flex">
+      <p>
+        <strong>Certified Medical Assistant</strong><br />
+        New Jersey (Central)<br />&nbsp;
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/FUk42cjjtX/Certified-Medical-Assistant-NJ"
+        target="_blank">Apply Now</a
+      >
+    </div>
+    
+    <div class="flow flex">
+      <p>
+        <strong>Certified Medical Assistant</strong><br />
+        New Jersey (North)<br />&nbsp;
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/d75Mgz5tz5/Certified-Medical-Assistant-Wound-Care-North-Jersey"
+        target="_blank">Apply Now</a
+      >
+    </div>
+
+    <div class="flow flex">
+      <p>
+        <strong>Nurse Practitioner</strong><br />
+        Wound Care - New Jersey (North)<br />
+        Salary: $140k - $150K - Full-time
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/d0j3AkepUQ/Nurse-Practitioner-Wound-Care-Northern-NJ"
+        target="_blank">Apply Now</a
+      >
+    </div>
+  </div>
 
   <h2>Georgia</h2>
-  <p>
-    <strong>Nurse Practitioner</strong><br />
-    Savannah, GA<br />
-    Salary $130,000 - $150,000 Annually
-  </p>
-  <a
-    href="https://restorefirsthealthduluth.applytojob.com/apply/3sAyKIkP2Z/Wound-Care-Nurse-Practitioner"
-    class="button">Apply Now</a
-  >
+  <div class="auto-grid georgia">
+    <div class="flow flex">
+      <p>
+        <strong>Nurse Practitioner</strong><br />
+        Savannah, GA<br />
+        Salary $130k - $150k Annually
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/3sAyKIkP2Z/Wound-Care-Nurse-Practitioner"
+        target="_blank">Apply Now</a
+      >
+    </div>
+  </div>
+
+  <h2>Pennsylvania</h2>
+  <div class="auto-grid pennsylvania">
+    <div class="flow flex">
+      <p>
+        <strong>Nurse Practitioner</strong><br />
+        Conshohocken, PA<br />
+        Salary $130k - $150k Annually
+      </p>
+      <a
+        class="button"
+        href="https://restorefirsthealthduluth.applytojob.com/apply/d1855UhLL9/Nurse-Practitioner-Wound-Care"
+        target="_blank">Apply Now</a
+      >
+    </div>
+  </div>
 </section>
+
+<style>
+  .auto-grid > div {
+    flex-direction: column;
+    padding: var(--size);
+    border: 1px solid lightgray;
+  }
+
+  .button {
+    margin-top: auto;
+  }
+
+  @media (min-width: 52em) {
+    .pennsylvania, .georgia {
+      max-width: 17.5em;
+    }
+  }
+</style>
