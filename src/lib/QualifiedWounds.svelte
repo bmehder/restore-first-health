c57762a64785e058165f0cb4f957d949c98c0cf1
bmehder <bmehder@gmail.com>
2024-01-30 01:58:31 +0000

    QualifiedWounds container query

--- src/lib/QualifiedWounds.svelte
+++ src/lib/QualifiedWounds.svelte
@@ -2,30 +2,59 @@
 	import Arrows from "$lib/icons/Arrows.svelte"
 </script>
 
-<div class="wound-qualifications">
-	<div></div>
-	<div class="wound-qualifications-item">Is larger than 2cm x 2cm</div>
-	<div></div>
-	<div class="arrow left"><Arrows /></div>
-	<div></div>
-	<div class="arrow right"><Arrows /></div>
-	<div class="wound-qualifications-item">Is an open wound on the skin</div>
-	<div class="arrow bottom"><Arrows /></div>
-	<!-- <div class="wound-qualifications-item">Failure of traditional treatments over 30-days</div> -->
-	<!-- <div class="wound-qualifications-item">Fails traditional treatments over 30 days</div> -->
-	<div class="wound-qualifications-item">Fails treatments over 30 days</div>
+<div class="container">
+	<div class="wound-qualifications">
+		<!-- 1 -->
+		<div></div>
+
+		<!-- 2 -->
+		<div class="item">
+			Is larger than 2cm x 2cm
+		</div>
+
+		<!-- 3 -->
+		<div></div>
+
+		<!-- 4 -->
+		<div class="arrow left">
+			<Arrows />
+		</div>
+
+		<!-- 5 -->
+		<div></div>
+
+		<!-- 6 -->
+		<div class="arrow right">
+			<Arrows />
+		</div>
+
+		<!-- 7 -->
+		<div class="item">
+			Is an open wound on the skin
+		</div>
+
+		<!-- 8 -->
+		<div class="arrow bottom">
+			<Arrows />
+		</div>
+
+		<!-- 9 -->
+		<div class="item">
+			Fails treatments over 30 days
+		</div>
+	</div>
 </div>
+
 
 <style>
   .wound-qualifications {
-    margin-inline: auto;
 		text-align: center;
     border-radius: var(--size);
 	}
 	
-	.wound-qualifications-item {
+	.wound-qualifications .item {
 		padding: 1.5rem;
-		background-color: #003a64;
+		background-color: var(--accent);
 		color: white;
 		border-radius: 0.5rem;
 		text-wrap: balance;
@@ -33,6 +62,7 @@
 
 	.wound-qualifications .arrow {
 		margin-block: 1.5rem;
+		line-height: 0;
 		rotate: 90deg;
 	}
 	
@@ -40,7 +70,7 @@
 		display: none;
 	}
 
-	@media screen and (min-width: 32rem) {
+	@container (min-width: 24rem) {
 		.wound-qualifications {
 			display: grid;
 			place-items: center;
@@ -49,7 +79,7 @@
 		}
 		
 		.wound-qualifications .left {
-			display: block;
+			display: block; /* hidden on mobile */
 			justify-self: end;
 			rotate: -45deg;
 		}
