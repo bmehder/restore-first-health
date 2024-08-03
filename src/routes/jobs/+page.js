d50d0ea5fcf4ec2a1a9bb029168ec86277399ca5
bmehder <bmehder@gmail.com>
2024-08-03 16:46:56 +0100

    removed +page.js for Jobs route

--- src/routes/jobs/+page.js
+++ src/routes/jobs/+page.js
@@ -1,15 +1,15 @@
-export async function load({ fetch, parent }) {
-	const { regions } = await parent()
+// export async function load({ fetch, parent }) {
+// 	const { regions } = await parent()
 
-	const jobsByRegions = Promise.all(
-		regions.map(({ id }) =>
-			fetch(`https://rfh-api.com/wp-json/wp/v2/jobs?per_page=100&regions=${id}`)
-				.then(x => x.json())
-				.catch(console.error)
-		)
-	)
+// 	const jobsByRegions = Promise.all(
+// 		regions.map(({ id }) =>
+// 			fetch(`https://rfh-api.com/wp-json/wp/v2/jobs?per_page=100&regions=${id}`)
+// 				.then(x => x.json())
+// 				.catch(console.error)
+// 		)
+// 	)
 
-	return {
-		jobsByRegions,
-	}
-}
+// 	return {
+// 		jobsByRegions,
+// 	}
+// }
