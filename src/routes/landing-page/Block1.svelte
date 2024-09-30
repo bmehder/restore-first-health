2252c2afc97d966d35fd22c9f9e875a3156c8bd9
bmehder <bmehder@gmail.com>
2024-09-30 14:51:40 +0100

    added content in block 1 of landing page

--- src/routes/landing-page/Block1.svelte
+++ src/routes/landing-page/Block1.svelte
@@ -8,7 +8,7 @@
 			<div class="two-rows flow">
 				<div>
 					<p>SERVICES</p>
-					<h2 class="h1">Enter Your Headline Here</h2>
+					<h2 class="h1">Living with a chronic wound can be overwhelming.</h2>
 				</div>
 				<p class="pretty">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, a
@@ -19,22 +19,34 @@
 			<div class="flow">
 				<Youtube />
 				<h3>Service Name</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<p>
+					Restore First Health provides expert wound care in the comfort of the home.
+				</p>
 			</div>
 			<div class="flow">
 				<Youtube />
 				<h3>Service Name</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<p>
+					We ensure a smooth, stress-free transition from care facilities with our
+					specialized wound therapies and care coordination.
+				</p>
 			</div>
 			<div class="flow">
 				<Youtube />
 				<h3>Service Name</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<p>
+					Bringing a loved one home with a wound can be scary; managing everything on
+					your own is challenging. You don’t have to do this alone.
+				</p>
 			</div>
 			<div class="flow">
 				<Youtube />
 				<h3>Service Name</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<p>
+					We come to your home, allowing you to focus on your loved one while we
+					handle the logistics of wound care, keeping you informed every step of the
+					way.
+				</p>
 			</div>
 		</div>
 	</div>
