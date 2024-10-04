acc3c34f372a1e55023939ae10d4f10338077a82
bmehder <bmehder@gmail.com>
2024-10-04 16:39:55 +0100

    show only published posts

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,6 +1,6 @@
 export const load = async ({ fetch }) => {
 	const endpoint =
-		'https://rfhapi.wpenginepowered.com/wp-json/wp/v2/posts?_embed&per_page=100'
+		'https://rfhapi.wpenginepowered.com/wp-json/wp/v2/posts?_embed&per_page=100&status=publish'
 
 	const response = await fetch(endpoint)
 	const posts = await response.json()
